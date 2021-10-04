9adf54e5cc80ec584c498bf109f1e1199468552d
Adam Berger <adam@ratchetdesigns.com>
2021-10-04 16:51:01 -0400

    feat: add from_serde_or_undefined and IntoSerdeOrDefault

--- ts-bindgen-rt/src/lib.rs
+++ ts-bindgen-rt/src/lib.rs
@@ -1,6 +1,9 @@
 extern crate js_sys;
 extern crate wasm_bindgen;
 
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::JsValue;
+
 pub struct Null;
 
 pub struct Undefined;
@@ -11,8 +14,8 @@ pub trait ShouldSkipSerializing {
 
 pub struct Error;
 
-impl From<Error> for wasm_bindgen::JsValue {
-    fn from(_: Error) -> wasm_bindgen::JsValue {
+impl From<Error> for JsValue {
+    fn from(_: Error) -> JsValue {
         js_sys::Error::new("rust failure").into()
     }
 }
@@ -20,5 +23,34 @@ impl From<Error> for wasm_bindgen::JsValue {
 impl From<serde_json::Error> for Error {
     fn from(_: serde_json::Error) -> Error {
         Error
+    }
+}
+
+pub trait IntoSerdeOrDefault {
+    fn into_serde_or_default<T>(&self) -> serde_json::Result<T>
+    where
+        T: Default + for<'a> Deserialize<'a>;
+}
+
+impl IntoSerdeOrDefault for JsValue {
+    fn into_serde_or_default<T>(&self) -> serde_json::Result<T>
+    where
+        T: Default + for<'a> Deserialize<'a>,
+    {
+        if self.is_undefined() {
+            Ok(T::default())
+        } else {
+            self.into_serde()
+        }
+    }
+}
+
+pub fn from_serde_or_undefined<T: ShouldSkipSerializing + Serialize>(
+    val: &T,
+) -> serde_json::Result<JsValue> {
+    if val.should_skip_serializing() {
+        Ok(JsValue::undefined())
+    } else {
+        JsValue::from_serde(val)
     }
 }
