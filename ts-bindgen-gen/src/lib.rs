d0fa6e1f203f7bad5f035233d1f8729a9ba03e61
Adam Berger <adam@ratchetdesigns.com>
2021-11-20 16:27:06 -0500

    refactor: make codegen directory

--- ts-bindgen-gen/src/lib.rs
+++ ts-bindgen-gen/src/lib.rs
@@ -14,7 +14,7 @@ use quote::quote;
 pub fn generate_rust_for_typescript<S: AsRef<str>>(module: S) -> TokenStream2 {
     let tt = TsTypes::try_new(module.as_ref()).expect("tt error");
     let tbnbf = tt.types_by_name_by_file;
-    let final_ir = to_final_ir(tbnbf.clone());
+    let final_ir = to_final_ir(tbnbf);
     let mod_def: ModDef = (&*final_ir.borrow()).into();
 
     quote! { #mod_def }
