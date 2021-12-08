a88449298be41b0cc5a8988503cb71bfb09aca08
Adam Berger <adam@ratchetdesigns.com>
2021-12-08 01:04:36 -0500

    test: stop writing an output file from basic test

--- ts-bindgen-gen/tests/basic_test.rs
+++ ts-bindgen-gen/tests/basic_test.rs
@@ -5,8 +5,6 @@ use ts_bindgen_gen::generate_rust_for_typescript;
 #[test]
 fn basic() -> std::io::Result<()> {
     let r = generate_rust_for_typescript("./tests/examples/basic");
-    let mut file = File::create("output.rs")?;
-    file.write_all(r.to_string().as_bytes())?;
-    println!("R {:?}", r);
+    // just making sure we don't crash for now...
     Ok(())
 }
