59ff9dcfda6552fe5cdaa5675e04a1f5f857a525
Adam Berger <adam@ratchetdesigns.com>
2022-01-09 20:53:49 -0500

    WIP: adding example

---
+++ ts-bindgen/examples/paperjs/a.d.ts
@@ -0,0 +1,6 @@
+export class A {}
+
+export class B {
+	n: number;
+	a: typeof A;
+}
---
+++ ts-bindgen/examples/paperjs/build.rs
@@ -0,0 +1,5 @@
+// npm install
+// cargo run --manifest-path ../../Cargo.toml --package ts-bindgen -- --output ./src/paper.rs --rerun-if-changed paper
+
+fn main() {
+}
