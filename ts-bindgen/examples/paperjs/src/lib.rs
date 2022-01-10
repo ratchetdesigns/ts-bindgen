73f86cc7964d79e8b32fde6271f32875e02c22b7
Adam Berger <adam@ratchetdesigns.com>
2022-01-09 20:53:49 -0500

    feat: add example

---
+++ ts-bindgen/examples/paperjs/src/lib.rs
@@ -0,0 +1,14 @@
+mod paper;
+
+fn main() {
+}
+
+#[cfg(test)]
+mod test {
+    extern crate wasm_bindgen_test;
+    use wasm_bindgen_test::*;
+
+    #[wasm_bindgen_test]
+    fn basic_test() {
+    }
+}
