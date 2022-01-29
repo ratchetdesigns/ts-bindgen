ratchetdesigns/ts-bindgen

commit 5634efb9672fe0679b5f3ea202fa06be79ccbe5c

fix: fix handling of JsValue enum cases

ts-bindgen-gen/src/codegen/mod.rs
@@ -537,29 +537,52 @@ impl<'a, FS: Fs + ?Sized> ToTokens for WithFs<'a, TargetEnrichedType, FS> {
                 // deserialize unions into the "larger" variant in case of overlaps
                 not_undefined_members.sort_by_key(|m| get_field_count(*m));
                 not_undefined_members.reverse();
-                let member_cases = not_undefined_members
+                let member_cases_by_name = not_undefined_members
                     .iter()
                     .map(|t| {
+                        let attrs = if t.serialization_type() == SerializationType::JsValue {
+                            quote! {
+                                #[serde(
+                                    serialize_with = "ts_bindgen_rt::serialize_jsvalue",
+                                    deserialize_with = "ts_bindgen_rt::deserialize_as_jsvalue")
+                                ]
+                            }
+                        } else {
+                            Default::default()
+                        };
                         let case = type_to_union_case_name(t);
 
                         if t.is_uninhabited() {
-                            quote! {
-                                #case
-                            }
+                            (
+                                case.clone(),
+                                quote! {
+                                    #attrs
+                                    #case
+                                },
+                            )
                         } else {
-                            quote! {
-                                #case(#t)
-                            }
+                            (
+                                case.clone(),
+                                quote! {
+                                    #attrs
+                                    #case(#t)
+                                },
+                            )
                         }
                     })
                     .chain(undefined_members.iter().map(|t| {
                         let case = type_to_union_case_name(t);
 
-                        quote! {
-                            #[serde(serialize_with="ts_bindgen_rt::serialize_undefined", deserialize_with="ts_bindgen_rt::deserialize_undefined")]
-                            #case
-                        }
-                    }));
+                        (
+                            case.clone(),
+                            quote! {
+                                #[serde(serialize_with="ts_bindgen_rt::serialize_undefined", deserialize_with="ts_bindgen_rt::deserialize_undefined")]
+                                #case
+                            },
+                        )
+                    }))
+                    .collect::<HashMap<_, _>>(); // to de-dupe names
+                let member_cases = member_cases_by_name.values();
 
                 quote! {
                     #[derive(Clone, serde::Serialize, serde::Deserialize)]

ts-bindgen-gen/src/codegen/funcs.rs
@@ -344,7 +344,7 @@ pub mod fn_types {
             .unwrap_or(SerializationType::SerdeJson);
         let typ = with_context(typ, in_context);
         match serialization_type {
-            SerializationType::Raw => quote! { #typ },
+            SerializationType::Raw | SerializationType::JsValue => quote! { #typ },
             SerializationType::SerdeJson => quote! { JsValue },
             SerializationType::Fn => {
                 let target = typ.resolve_target_type();
@@ -413,7 +413,9 @@ pub mod fn_types {
         let typ = with_context(typ, in_context);
         let typ = typ.as_ref();
         match serialization_type {
-            SerializationType::Raw | SerializationType::SerdeJson => quote! { #typ },
+            SerializationType::Raw | SerializationType::SerdeJson | SerializationType::JsValue => {
+                quote! { #typ }
+            }
             SerializationType::Fn => match typ {
                 // TODO: fix this leaky abstraction...
                 TypeRefLike::TypeRef(_) => quote! { &'static #typ },
@@ -914,7 +916,7 @@ impl<T: WrappedParam> ParamExt for T {
         let name = self.rust_name();
 
         match serialization_type {
-            SerializationType::Raw => quote! { #name },
+            SerializationType::Raw | SerializationType::JsValue => quote! { #name },
             SerializationType::SerdeJson => {
                 quote! {
                     ts_bindgen_rt::from_jsvalue(&#name).map_err(ts_bindgen_rt::Error::from)?
@@ -1154,7 +1156,7 @@ fn render_wasm_bindgen_return_to_js(
 ) -> TokenStream2 {
     let serialization_type = return_type.serialization_type();
     match serialization_type {
-        SerializationType::Raw => return_value.clone(),
+        SerializationType::Raw | SerializationType::JsValue => return_value.clone(),
         SerializationType::SerdeJson => {
             if is_fallible {
                 quote! {
@@ -1184,7 +1186,7 @@ fn render_wasm_bindgen_return_to_js(
 pub fn render_raw_return_to_js(return_type: &TypeRef, return_value: &TokenStream2) -> TokenStream2 {
     let serialization_type = return_type.serialization_type();
     match serialization_type {
-        SerializationType::Raw => quote! {
+        SerializationType::Raw | SerializationType::JsValue => quote! {
             #return_value.into_serde().unwrap()
         },
         SerializationType::SerdeJson => {
@@ -1211,7 +1213,7 @@ fn render_rust_to_js_conversion(
 ) -> (bool, TokenStream2) {
     let serialization_type = typ.serialization_type();
     match serialization_type {
-        SerializationType::Raw => (false, quote! { #name }),
+        SerializationType::Raw | SerializationType::JsValue => (false, quote! { #name }),
         SerializationType::SerdeJson => (
             true,
             quote! { ts_bindgen_rt::to_jsvalue(&#name)#error_mapper },
@@ -1228,7 +1230,7 @@ fn render_rust_to_jsvalue_conversion(
 ) -> TokenStream2 {
     let serialization_type = typ.serialization_type();
     match serialization_type {
-        SerializationType::Raw => quote! { JsValue::from(#name) },
+        SerializationType::Raw | SerializationType::JsValue => quote! { JsValue::from(#name) },
         SerializationType::SerdeJson => {
             quote! { ts_bindgen_rt::to_jsvalue(&#name)#error_mapper }
         }

ts-bindgen-gen/src/codegen/named.rs
@@ -26,6 +26,7 @@ pub trait Named {
 
 impl Named for Builtin {
     fn to_name(&self) -> (&str, Identifier) {
+        // TODO: anything that returns JsValue here should have SerializationType::JsValue
         match self {
             Builtin::PrimitiveAny => ("JsValue", to_ident("JsValue")),
             Builtin::PrimitiveNumber => ("f64", to_ident("f64")),

ts-bindgen-gen/src/codegen/serialization_type.rs
@@ -7,6 +7,7 @@ pub enum SerializationType {
     Raw,
     SerdeJson,
     Fn,
+    JsValue,
 }
 
 pub trait SerializationTypeGetter {
@@ -30,6 +31,9 @@ impl SerializationTypeGetter for TargetEnrichedTypeInfo {
             TargetEnrichedTypeInfo::Ref(t) => match &t.referent {
                 TypeIdent::Builtin(Builtin::Fn) => SerializationType::Fn,
                 TypeIdent::Builtin(Builtin::Array) => SerializationType::SerdeJson,
+                TypeIdent::Builtin(Builtin::PrimitiveAny | Builtin::PrimitiveObject) => {
+                    SerializationType::JsValue
+                }
                 TypeIdent::Builtin(_) => SerializationType::Raw,
                 _ => SerializationType::SerdeJson,
             },

ts-bindgen-rt/src/lib.rs
@@ -98,6 +98,15 @@ where
     serializer.serialize_newtype_struct(JSVALUE_NEWTYPE_STRUCT, &idx)
 }
 
+pub fn serialize_jsvalue<S, V>(value: &V, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: ser::Serializer,
+    V: Clone + IntoWasmAbi<Abi = u32>,
+{
+    let idx = value.clone().into_abi();
+    serializer.serialize_newtype_struct(JSVALUE_NEWTYPE_STRUCT, &idx)
+}
+
 pub fn serialize_undefined<S>(serializer: S) -> Result<S::Ok, S::Error>
 where
     S: ser::Serializer,

commit 292703c190cf53bd6973ddc50191b3799d54c815

fix: fix ordering of union cases, bug introduced in 5634efb9672fe0679b5f3ea202fa06be79ccbe5c

ts-bindgen-gen/src/codegen/mod.rs
@@ -537,7 +537,7 @@ impl<'a, FS: Fs + ?Sized> ToTokens for WithFs<'a, TargetEnrichedType, FS> {
                 // deserialize unions into the "larger" variant in case of overlaps
                 not_undefined_members.sort_by_key(|m| get_field_count(*m));
                 not_undefined_members.reverse();
-                let member_cases_by_name = not_undefined_members
+                let member_cases = not_undefined_members
                     .iter()
                     .map(|t| {
                         let attrs = if t.serialization_type() == SerializationType::JsValue {
@@ -581,8 +581,16 @@ impl<'a, FS: Fs + ?Sized> ToTokens for WithFs<'a, TargetEnrichedType, FS> {
                             },
                         )
                     }))
-                    .collect::<HashMap<_, _>>(); // to de-dupe names
-                let member_cases = member_cases_by_name.values();
+                    .fold(
+                        (HashSet::new(), Vec::new()),
+                        |(mut names, mut cases), (name, case)| {
+                            // de-dupe names
+                            if names.insert(name) {
+                                cases.push(case);
+                            }
+                            (names, cases)
+                        }
+                    ).1;
 
                 quote! {
                     #[derive(Clone, serde::Serialize, serde::Deserialize)]