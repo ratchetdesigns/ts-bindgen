eeeb7b985a876b252cdcc9ffbdb998cb11262374
Adam Berger <adam@ratchetdesigns.com>
2022-01-31 02:22:12 -0500

    build: update to custom rustfmt-nightly that builds for wasm

--- ts-bindgen-web/src/fmt.rs
+++ ts-bindgen-web/src/fmt.rs
@@ -1,14 +1,19 @@
-use ::rustfmt::{format, Config, Input};
+use ::rustfmt::{Config, EmitMode, Input, Session, Verbosity};
 use std::error::Error;
 
 pub fn rust_fmt(s: String) -> Result<String, Box<dyn Error>> {
-    let config = Config::default();
-    let report = format(Input::Text(s), &config, Default::default())?;
-    let result = report
-        .format_result()
-        .next()
-        .map(|(_, res)| res.formatted_text())
-        .unwrap_or_default();
+    let config = {
+        let mut config = Config::default();
+        config.set().emit_mode(EmitMode::Stdout);
+        config.set().verbose(Verbosity::Quiet);
+        config
+    };
+    let mut buf: Vec<u8> = Default::default();
 
-    Ok(result.to_owned())
+    {
+        let mut session = Session::new(config, Some(&mut buf));
+        session.format(Input::Text(s))?;
+    }
+
+    Ok(String::from_utf8(buf)?)
 }
