7a40b947d557766dc6ac3e60ef789451e81745df
Adam Berger <adam@ratchetdesigns.com>
2022-02-19 03:26:15 -0500

    doc: update release.sh

--- release.sh
+++ release.sh
@@ -13,7 +13,7 @@ if [ "${good}" != "y" ]; then
 fi
 
 cargo workspaces version --yes --all --no-git-push --no-git-tag --exact custom "${version}"
-echo "Update ts-bindgen-web Cargo.toml and README.md to use ${next_version}"
+echo "Update README.md, ts-bindgen-web/Cargo.toml, ts-bindgen-gen/tests/end_to_end_template/Cargo.toml, and ts-bindgen/examples/paper/Cargo.toml to use ${version}, cargo generate-lockfile, and git commit --amend"
 
 echo "Ready to proceed (y/n)? "
 read good
@@ -26,4 +26,6 @@ git tag "v${version}"
 
 cargo workspaces version --yes --all --no-git-push --exact --no-git-tag --force '*' custom "${next_version}"
 
+echo "Update ts-bindgen-web, Cargo.toml, and README.md to use ${next_version}, cd ts-bindgen-web, cargo generate-lockfile, and git commit --amend"
+echo "Update README.md, ts-bindgen-web/Cargo.toml, ts-bindgen-gen/tests/end_to_end_template/Cargo.toml, and ts-bindgen/examples/paper/Cargo.toml to use ${next_version}, cargo generate-lockfile, and git commit --amend"
 echo "Run: git checkout -b scratch v${version} && cd ts-bindgen-web && <update Cargo.toml> && ./build && wrangler publish"
