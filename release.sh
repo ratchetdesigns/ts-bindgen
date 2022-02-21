071c68164031aa02830098ca11385a7c75e44b2a
Adam Berger <adam@ratchetdesigns.com>
2022-02-21 13:18:53 -0500

    build: automate most of versioning with cargo-edit

--- release.sh
+++ release.sh
@@ -1,10 +1,16 @@
 set -e
 
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <version> <next-version>"
+  exit 2
+fi
+
 version="$1"
 next_version="$2"
 
-cat CHANGELOG.md
+head CHANGELOG.md
 
+echo "Using version ${version} and next version ${next_version}"
 echo "Confirm changelog looks good (y/n)? "
 read good
 
@@ -12,20 +18,52 @@ if [ "${good}" != "y" ]; then
   exit 1
 fi
 
-cargo workspaces version --yes --all --no-git-push --no-git-tag --exact custom "${version}"
-echo "Update README.md, ts-bindgen-web/Cargo.toml, ts-bindgen-gen/tests/end_to_end_template/Cargo.toml, and ts-bindgen/examples/paper/Cargo.toml to use ${version}, cargo generate-lockfile, and git commit --amend"
+make_version() {
+  local readonly version="$1"
 
-echo "Ready to proceed (y/n)? "
-read good
+  # requires cargo-edit
 
-if [ "${good}" != "y" ]; then
-  exit 1
-fi
+  # set our workspace versions
+  cargo set-version --workspace "${version}"
+  # todo: why isn't this changed as part of set-version???
+  cargo add --manifest-path "$(pwd)/ts-bindgen-macro/Cargo.toml" --path ts-bindgen-gen "ts-bindgen-gen@${version}"
 
-git tag "v${version}"
+  # set our end_to_end_template version
+  cp -r ts-bindgen-rt ts-bindgen-gen/tests/end_to_end_template/
+  cargo add --manifest-path "$(pwd)/ts-bindgen-gen/tests/end_to_end_template/Cargo.toml" --path ts-bindgen-gen/tests/end_to_end_template/ts-bindgen-rt "ts-bindgen-rt@${version}"
+  cargo generate-lockfile --manifest-path "$(pwd)/ts-bindgen-gen/tests/end_to_end_template/Cargo.toml"
+  rm -rf ts-bindgen-gen/tests/end_to_end_template/ts-bindgen-rt
 
-cargo workspaces version --yes --all --no-git-push --exact --no-git-tag --force '*' custom "${next_version}"
+  # set our ts-bindgen-web version
+  cargo set-version --manifest-path "$(pwd)/ts-bindgen-web/Cargo.toml" "${version}"
+  cargo add --manifest-path "$(pwd)/ts-bindgen-web/Cargo.toml" --no-default-features --path ts-bindgen "ts-bindgen@${version}"
+  cargo generate-lockfile --manifest-path "$(pwd)/ts-bindgen-web/Cargo.toml"
 
-echo "Update ts-bindgen-web, Cargo.toml, and README.md to use ${next_version}, cd ts-bindgen-web, cargo generate-lockfile, and git commit --amend"
-echo "Update README.md, ts-bindgen-web/Cargo.toml, ts-bindgen-gen/tests/end_to_end_template/Cargo.toml, and ts-bindgen/examples/paper/Cargo.toml to use ${next_version}, cargo generate-lockfile, and git commit --amend"
-echo "Run: git checkout -b scratch v${version} && cd ts-bindgen-web && <update Cargo.toml> && ./build && wrangler publish"
+  # set paperjs example version
+  cargo set-version --manifest-path "$(pwd)/ts-bindgen/examples/paperjs/Cargo.toml" "${version}"
+  cargo add --manifest-path "$(pwd)/ts-bindgen/examples/paperjs/Cargo.toml" --path ts-bindgen-rt "ts-bindgen-rt@${version}"
+  cargo generate-lockfile --manifest-path "$(pwd)/ts-bindgen/examples/paperjs/Cargo.toml"
+
+  echo "Update README.md versions"
+
+  echo "Ready to proceed (y/n)? "
+  read good
+
+  if [ "${good}" != "y" ]; then
+    exit 1
+  fi
+
+  cargo test
+
+  if [[ "${version}" =~ ".*-pre" ]]; then
+    git commit -a -m "chore: prepare ${version}"
+  else
+    git commit -a -m "chore: release ${version}"
+    git tag "v${version}"
+  fi
+}
+
+make_version "${version}"
+make_version "${next_version}"
+
+echo "Run: git checkout v${version} && cd ts-bindgen-web && ./build && wrangler publish"
